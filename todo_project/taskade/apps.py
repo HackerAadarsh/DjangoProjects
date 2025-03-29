30cb91e6e41a0d8e80d193d99e4becdeb65a5d1b
Aadarsh Mishra <aadarshmishra2024@gmail.com>
2025-03-29 22:22:38 +0530

    Reorganized project structure

--- todo_project/taskade/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TaskadeConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "taskade"
--- todo_project/taskade/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import TODOO
-
-class TaskForm(forms.ModelForm):
-    class Meta:
-        model = TODOO
-        fields = ['title', 'description', 'completed']
